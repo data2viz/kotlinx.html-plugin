7b67cdc52be75349c37094fb0a4215f90e453d53
gzoritchak <g.zoritchak@gmail.com>
2019-04-02 23:25:53 +0200

    Removing java code.

--- src/main/java/io/data2viz/kotlinx/htmlplugin/ide/controller/ConvertTextHTMLCopyPasteProcessor.java
+++
@@ -1,7 +0,0 @@
-package io.data2viz.kotlinx.htmlplugin.ide.controller;
-
-// Hack, because Kotlin files not works as reference in plugin.xml
-public class ConvertTextHTMLCopyPasteProcessor extends ConvertTextHTMLCopyPasteProcessorKt {
-
-
-}
